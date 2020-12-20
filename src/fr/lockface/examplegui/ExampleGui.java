bcb59bad4d1e67ace53eda57c9df7349877dcb82
Nicolas Went <nicolas.went@epita.fr>
2020-12-20 13:37:28 +0100

    [Archi] First commit

---
+++ src/fr/lockface/examplegui/ExampleGui.java
@@ -0,0 +1,16 @@
+package fr.lockface.examplegui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ExampleGui extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
